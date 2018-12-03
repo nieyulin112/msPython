dc2a286d1c8d8bade32873135aecf84841e4d03e
ppd-03020225 <ppd-03020225@ppd-03020225deMacBook-Pro.local>
2018-12-03 13:39:41 +0800

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+# list
+l = [1, 2, 3, 4]
+print l
+for val in l:
+    print val
+# list function
+l.append(5)
+print l
+l.insert(1, 10)
+print l
+l.pop()
+l.pop(1)
+print l
---
+++ tuple.py
@@ -0,0 +1,2 @@
+t = ('admin', 'scotter', 'Lisa')
+print t
